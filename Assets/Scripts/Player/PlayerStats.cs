d205b01da03a290910e0a14131bfe328891a91af
Gevorg <gulkanyangk@mail.ru>
2021-07-28 20:40:34 +0300

    fix

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,10 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 namespace WiresGame.Player
 {
     public class PlayerStats
     {
         private string _name;
+        private int _score;
+
+        public int Score => _score;
+
+        public void AddScore(int score)
+        {
+            if (score < 0)
+                new ArgumentOutOfRangeException(nameof(score) + " < 0");
+
+            _score += score;
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
     }
 }
